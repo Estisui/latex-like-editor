d45e5ec811a82aef18e9e999499fd14939e05eae
Stepan Sukhachev <droiderlinux@yahoo.com>
2024-12-09 14:09:13 +0300

    feat: add toaster to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import AppRoutes from './routes';
+import { Toaster } from './components/ui/toaster';
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <AppRoutes />
+      <Toaster />
     </AuthProvider>
   );
 };
